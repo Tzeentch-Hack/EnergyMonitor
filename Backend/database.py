55d12cc7b68c02267158a4ada53e2a26b20c2858
SherAlex1998 <62588294+SherAlex1998@users.noreply.github.com>
2023-11-10 19:34:08 +0500

    Added backend project and authorization system

---
+++ Backend/database.py
@@ -0,0 +1,48 @@
+import sqlalchemy.sql.sqltypes
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, BOOLEAN, JSON
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./databases/users.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+Base = sqlalchemy.orm.declarative_base()
+
+
+class UserInDBSQL(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    disabled = Column(BOOLEAN)
+    hashed_password = Column(String)
+
+
+class RequestCounter(Base):
+    __tablename__ = "request_counter"
+
+    id = Column(Integer, primary_key=True)
+    request_count = Column(Integer, default=0)
+
+    def to_dict(self):
+        return {
+            'request_count': self.request_count,
+        }
+
+
+Base.metadata.create_all(bind=engine)
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+db = SessionLocal()
+
+
+if not db.query(RequestCounter).filter(RequestCounter.id == 1).first():
+    initial_request_counter = RequestCounter(request_count=0)
+    db.add(initial_request_counter)
+    db.commit()
+
+app = None
